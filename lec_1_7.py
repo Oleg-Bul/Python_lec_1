399e39219c546644187b7a8d5e7165c56b97aa08
Oleg_bul <kokseg63@gmail.com>
2022-11-20 17:13:47 +0400

    lec1_7,8,9,10 added

--- lec_1_7.py
+++ lec_1_7.py
@@ -1,1 +1,10 @@
 # Работа со строками
+
+text = 'сьешь ещё этих мягких французких булок'
+# print(len(text))                        #39 (длина строки)
+# print('ещё' in text)                    # True (поиск)
+# print(text.isdigit())                   # False (проверка на цифры)
+# print(text.islower())                   # True (хз, наверно на текст проверка)
+# print(text.replace('ещё', 'ЕЩЁ'))       # Замена
+
+ 
---
+++ lec_1_9.py
@@ -0,0 +1,10 @@
+colors = ['red', 'green','blue']
+for e in colors:
+    print(e)       # red green blue
+
+for e in colors:
+    print(e*2)     #redred greengreen blueblue
+
+colors.append('gray') # добавть в конец
+print (colors == ['red', 'green', 'blue', 'gray']) #True
+colors.remove('red')  # del colors [0] # удалить элемент
